f26c0bbe72587fb8c44e0b9f9a09bffa3e2c91b1
Nelson-Sosa <ns5389188@gmail.com>
2026-04-09 10:37:32 -0300

    "Fix CORS and API URL"

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://codigo-py-flax.vercel.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.routes'));
